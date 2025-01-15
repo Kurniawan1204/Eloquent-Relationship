c909efe206f5952cc85f30981fc5479a0db3a755
Kurniawan1204 <kurniawanebot12@gmail.com>
2025-01-15 10:29:15 +0700

    Membuatan model dan migration baru

--- eloquent-relationships/resources/views/users.blade.php
+++ eloquent-relationships/resources/views/users.blade.php
@@ -4,8 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Eloquent Relationships : Relasi One to One</title>
-    <link rel="stylesheet" type="text/css" href="animate.css">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
 </head>
@@ -13,8 +11,6 @@
     <div class="container">
         <div class="card mt-5">
             <div class="card-body">
-                <h1 class="animated bounceInUp">WWW.MALASNGODING.COM</h1>
-
                 <h3 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h3>
                 <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One</h5>
                 <table class="table table-borderles text-center table-striped table-warning">
