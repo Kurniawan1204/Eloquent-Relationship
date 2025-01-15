c33a17f3dae1979f0b2c2e401fc763c784c4cfbe
Kurniawan1204 <kurniawanebot12@gmail.com>
2025-01-15 10:23:07 +0700

    Menampilkan data di View

---
+++ eloquent-relationships/resources/views/users.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Eloquent Relationships : Relasi One to One</title>
+    <link rel="stylesheet" type="text/css" href="animate.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body">
+                <h1 class="animated bounceInUp">WWW.MALASNGODING.COM</h1>
+
+                <h3 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h3>
+                <h5 class="text-center my-4">Laravel Eloquent Relationship : One To One</h5>
+                <table class="table table-borderles text-center table-striped table-warning">
+                    <thead>
+                        <tr>
+                            <th>Nama User</th>
+                            <th>Nomor Telepon</th>
+                        </tr>
+                        <tr>
+                            <td>KURNIAWAN</td>
+                            <td>081394740078</td>
+                        </tr>
+                        <tr>
+                            <td>Yudi Purwanto</td>
+                            <td>081200046719</td>
+                        </tr>
+                        <tr>
+                            <td>Rizqi Maulana</td>
+                            <td>0892223918038</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->phone->phone }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
